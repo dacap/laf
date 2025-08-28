3f1f86cc734443ba5c72d25c72cdf41ca208e9fe
David Capello <david@igara.com>
2025-08-28 11:50:41 -0300

    Fix proper color profile description for sRGB profiles (fix aseprite/aseprite#5297)
    We were using the get_color_profile_description() function defined in
    Skia m102 but after the migration to m124 we lost the function. Here
    we're just bringing it back from Skia m102.

--- os/skia/skia_color_space.cpp
+++ os/skia/skia_color_space.cpp
@@ -21,12 +21,90 @@
 
 namespace os {
 
+namespace {
+
 // Copied from skia/src/core/SkColorSpacePriv.h
-static constexpr float gSRGB_toXYZD50[]{
+constexpr float gSRGB_toXYZD50[]{
   0.4360747f, 0.3850649f, 0.1430804f, // Rx, Gx, Bx
   0.2225045f, 0.7168786f, 0.0606169f, // Ry, Gy, By
   0.0139322f, 0.0971045f, 0.7141733f, // Rz, Gz, Bz
 };
+
+// Code for get_color_profile_description() copied from
+// skia/src/core/SkICC.cpp from Skia m102
+
+bool nearly_equal(float x, float y)
+{
+  // A note on why I chose this tolerance:  transfer_fn_almost_equal() uses a
+  // tolerance of 0.001f, which doesn't seem to be enough to distinguish
+  // between similar transfer functions, for example: gamma2.2 and sRGB.
+  //
+  // If the tolerance is 0.0f, then this we can't distinguish between two
+  // different encodings of what is clearly the same colorspace.  Some
+  // experimentation with example files lead to this number:
+  static constexpr float kTolerance = 1.0f / (1 << 11);
+  return ::fabsf(x - y) <= kTolerance;
+}
+
+bool nearly_equal(const skcms_TransferFunction& u, const skcms_TransferFunction& v)
+{
+  return nearly_equal(u.g, v.g) && nearly_equal(u.a, v.a) && nearly_equal(u.b, v.b) &&
+         nearly_equal(u.c, v.c) && nearly_equal(u.d, v.d) && nearly_equal(u.e, v.e) &&
+         nearly_equal(u.f, v.f);
+}
+
+bool nearly_equal(const skcms_Matrix3x3& u, const skcms_Matrix3x3& v)
+{
+  for (int r = 0; r < 3; r++) {
+    for (int c = 0; c < 3; c++) {
+      if (!nearly_equal(u.vals[r][c], v.vals[r][c])) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+// Return nullptr if the color profile doen't have a special name.
+const char* get_color_profile_description(const skcms_TransferFunction& fn,
+                                          const skcms_Matrix3x3& toXYZD50)
+{
+  bool srgb_xfer = nearly_equal(fn, SkNamedTransferFn::kSRGB);
+  bool srgb_gamut = nearly_equal(toXYZD50, SkNamedGamut::kSRGB);
+  if (srgb_xfer && srgb_gamut) {
+    return "sRGB";
+  }
+  bool line_xfer = nearly_equal(fn, SkNamedTransferFn::kLinear);
+  if (line_xfer && srgb_gamut) {
+    return "Linear Transfer with sRGB Gamut";
+  }
+  bool twoDotTwo = nearly_equal(fn, SkNamedTransferFn::k2Dot2);
+  if (twoDotTwo && srgb_gamut) {
+    return "2.2 Transfer with sRGB Gamut";
+  }
+  if (twoDotTwo && nearly_equal(toXYZD50, SkNamedGamut::kAdobeRGB)) {
+    return "AdobeRGB";
+  }
+  bool display_p3 = nearly_equal(toXYZD50, SkNamedGamut::kDisplayP3);
+  if (srgb_xfer || line_xfer) {
+    if (srgb_xfer && display_p3) {
+      return "sRGB Transfer with Display P3 Gamut";
+    }
+    if (line_xfer && display_p3) {
+      return "Linear Transfer with Display P3 Gamut";
+    }
+    bool rec2020 = nearly_equal(toXYZD50, SkNamedGamut::kRec2020);
+    if (srgb_xfer && rec2020) {
+      return "sRGB Transfer with Rec-BT-2020 Gamut";
+    }
+    if (line_xfer && rec2020) {
+      return "Linear Transfer with Rec-BT-2020 Gamut";
+    }
+  }
+  return nullptr;
+}
+
+} // namespace
 
 SkiaColorSpace::SkiaColorSpace(const gfx::ColorSpaceRef& gfxcs) : m_gfxcs(gfxcs), m_skcs(nullptr)
 {
@@ -109,11 +187,10 @@ SkiaColorSpace::SkiaColorSpace(const gfx::ColorSpaceRef& gfxcs) : m_gfxcs(gfxcs)
     skcms_TransferFunction fn;
     skcms_Matrix3x3 toXYZD50;
     if (m_skcs->isNumericalTransferFn(&fn) && m_skcs->toXYZD50(&toXYZD50)) {
-#if 0 // TODO create a description for the color profile
+      // Create a description for the color profile
       const char* desc = get_color_profile_description(fn, toXYZD50);
       if (desc)
         m_gfxcs->setName(desc);
-#endif
     }
   }
 
